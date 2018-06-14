d6082449ca46b1403844320e0cd8e4a2d3b989f0
asavah <asavah@avh.od.ua>
2018-06-14 21:58:24 +0300

    search for vcgencmd binary fix addon.id typo bump required xbmc.python version get rid of changelog, use <news/> tag

--- resources/lib/gui.py
+++ resources/lib/gui.py
@@ -15,6 +15,7 @@ class Screensaver(xbmcgui.WindowXMLDialog):
     def onInit(self):
         self._is_powered = True
         self.Monitor = MyMonitor(action = self._power_on)
+        self.vcgencmd = self._find_vcgencmd()
         self._power_toggle()
 
     def _power_on(self):
@@ -22,12 +23,16 @@ class Screensaver(xbmcgui.WindowXMLDialog):
         self.close()
 
     def _power_toggle(self):
+        if not self.vcgencmd:
+            xbmc.log(msg="%s: vcgencmd is missing, consult your OS support forums" % ( __addonid__), level=xbmc.LOGERROR)
+            return False
+
         if self._is_powered:
             arg2 = '0'
         else:
             arg2 = '1'
         try:
-            cmd = subprocess.Popen(['vcgencmd', 'display_power', arg2], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            cmd = subprocess.Popen([self.vcgencmd, 'display_power', arg2], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
             (out, err) = cmd.communicate()
             xbmc.log(msg="%s: vcgencmd returned %s" % ( __addonid__, repr(out) ), level=xbmc.LOGDEBUG)
             if not err:
@@ -37,6 +42,36 @@ class Screensaver(xbmcgui.WindowXMLDialog):
         except:
             xbmc.log(msg="%s: Exception running vcgencmd" % ( __addonid__ ), level=xbmc.LOGERROR)
 
+    def _find_vcgencmd(self):
+        syspath = os.getenv("PATH", None)
+
+        common_paths = [
+            "/opt/vc/sbin",
+            "/opt/vc/bin",
+            "/usr/local/sbin",
+            "/usr/local/bin",
+            "/usr/sbin",
+            "/usr/bin",
+            "/sbin",
+            "/bin"
+        ]
+
+        if not syspath:
+            syssearch = []
+        else:
+            syssearch = syspath.split(":")
+
+        for cp in common_paths[::-1]:
+            if not cp in syssearch:
+                syssearch.insert(0, cp)
+
+        for sp in syssearch:
+            fvcg = os.path.join(sp, "vcgencmd")
+            if os.path.isfile(fvcg):
+                xbmc.log(msg="%s: found vcgencmd %s" % ( __addonid__, repr(fvcg) ), level=xbmc.LOGDEBUG)
+                return fvcg
+
+        return None
 
 class MyMonitor(xbmc.Monitor):
     def __init__( self, *args, **kwargs ):
